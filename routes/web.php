97602ac6dad2cd5c2712c95eb0080afc32c6fa79
enjelinasaruksuk <enjelinasaruksuk@gmail.com>
2025-03-24 15:35:03 +0700

    menambahkan login dan signup(registrasi)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use illuminate\http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/signup', function () {
+    return view('signup'); // Pastikan ada file resources/views/signup.blade.php
+});
+
+Route::post('/signup', function (Request $request) {
+    return back()->with('success', 'Sign up successful! (This is a dummy action)');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
