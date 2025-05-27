49d758ce052ccfa03839e40e4c0658a4da6394c5
chrstnestg <christinesitanggang5@gmail.com>
2025-05-27 22:24:40 +0700

    feat: tambah migration dan model Pengguna, fix error insert tanpa no_telepon

---
+++ database/migrations/2025_05_27_150421_create_pengguna_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('pengguna', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('nama', 100);
+            $table->string('nama_pengguna', 100);
+            $table->string('no_telepon', 20);
+            $table->text('alamat')->nullable();
+            $table->string('foto')->nullable();
+            $table->timestamps(); // ini otomatis buat created_at dan updated_at
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pengguna');
+    }
+};
