090344d71614cf77561e083174b6c6f256231ed2
auliasalsabilla <aulia.dharensaa12@gmail.com>
2025-06-10 06:20:22 +0700

    menambahkan model pesanan dan pesanan detail

---
+++ app/Models/Pesanan.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pesanan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pesanan';
+
+    protected $fillable = [
+        'user_id',
+        'nama_penerima',
+        'telepon',
+        'alamat',
+        'total',
+        'metode_pembayaran',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function detail()
+    {
+        return $this->hasMany(PesananDetail::class, 'pesanan_id');
+    }
+}
---
+++ app/Models/PesananDetail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PesananDetail extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pesanan_detail';
+
+    protected $fillable = [
+        'pesanan_id',
+        'produk_id',
+        'kuantitas',
+        'harga_satuan',
+        'subtotal',
+    ];
+
+    public function pesanan()
+    {
+        return $this->belongsTo(Pesanan::class, 'pesanan_id');
+    }
+
+    public function produk()
+    {
+        return $this->belongsTo(Produk::class, 'produk_id');
+    }
+}
